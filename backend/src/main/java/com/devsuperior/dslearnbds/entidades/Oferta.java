8129ea8804a8e398e421f3311b0f0a3d41b358e5
felipecabesssa <felipesoares100@hotmail.com>
2023-01-24 10:53:14 -0300

    Timezone - Oferta

--- backend/src/main/java/com/devsuperior/dslearnbds/entidades/Oferta.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entidades/Oferta.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,7 +22,9 @@ public class Oferta implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edicao;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant momentoInicial;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant momentoFinal;
 	
 	@ManyToOne
